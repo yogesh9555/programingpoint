12cc1f1db8d842ba16b46153d6c3d56a23a372c8
yogesh9555 <71551746+yogesh9555@users.noreply.github.com>
2021-02-24 02:31:27 +0530

    Changes

--- README.md
+++ README.md
@@ -17,6 +17,11 @@
          alt="Create React App version" />
   </a>
   
+  <a href="https://github.com/yogesh9555/programingpoint/pulls">
+    <img src="https://badgen.net/github/prs/yogesh9555/programingpoint" alt="Pull Request Amount">
+  </a>
+  <img src="https://badgen.net/github/last-commit/yogesh9555/programingpoint" alt="Last commit date" />
+            
   <a href="./LICENSE">
     <img src="https://badgen.net/badge/license/MIT/blue"
          alt="License">
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import theme from "theme";
 import { Theme, Link } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Override, Menu } from "@quarkly/components";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -13,6 +14,20 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
+		<Menu
+			display="flex"
+			justify-content="center"
+			font="--lead"
+			font-weight="700"
+			margin="-6px 0 16px"
+			md-flex-direction="column"
+			md-align-items="center"
+			text-align="left"
+		>
+			<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+			<Override slot="link-active" color="--primary" />
+			<Override slot="item" padding="6px 0px" />
+		</Menu>
 		<Link
 			font={"--capture"}
 			font-size={"10px"}
