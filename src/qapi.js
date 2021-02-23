f7f04b2561e6d25cfbcd40266edc0f3a10c845d0
yogesh9555 <71551746+yogesh9555@users.noreply.github.com>
2021-02-24 02:00:55 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"603565c0c3a5f1001e7b6c72",
+				"603565c0c3a5f1001e7b6c74"
+			]
+		},
+		"603565c0c3a5f1001e7b6c72": {
+			"id": "603565c0c3a5f1001e7b6c72",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"603565c0c3a5f1001e7b6c74": {
+			"id": "603565c0c3a5f1001e7b6c74",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
